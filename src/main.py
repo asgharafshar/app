656ee3f9467877232a016585ee39a6e73f7d7e55
asgharafshar <zarep.mhd1@gmail.com>
2025-07-19 17:44:30 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
 
+def main(context):
+    try:
+        data = context.req.json()
+        context.log("📥 داده دریافتی:", str(data))
+
+        return context.res.text("فانکشن اجرا شد")
+
+    except Exception as e:
+        context.error(f"❌ خطا: {str(e)}")
+        return context.res.text("خطا در اجرا")
